7c0bc629fbaa2da4b9d51de95564b87d5f3100f6
MoMI-Github <github@movingimage.org>
2025-07-29 15:32:27 -0400

    Update qr-code.js

--- public/qr-code.js
+++ public/qr-code.js
@@ -1192,7 +1192,7 @@ function renderBmpUri(matrix, options) {
 
 
 // Comment-out the following line to convert this into a non-module .js file (e.g. for use in a <script src> tag over the file: protocol)
-export default QrCode
+
 
 window.QrCode = QrCode;
 
